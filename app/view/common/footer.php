61b34c071778cacda97b44b413dff62bdafc8c5b
alexmx57 <alexmeux27@gmail.com>
2023-12-15 14:11:51 +0100

    Footer terminé

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -1,11 +1,16 @@
 <?php
 
+
+
+
+
 $footer = [
     "footer_left" =>
     [
         "Instagram" => 'https://www.instagram.com/eh_connard/?igshid=OGQ5ZDc2ODk2ZA%3D%3D',
         "TikTok" => 'https://www.tiktok.com/@eh_connard?_t=8hva9LT0cTI&_r=1',
         "Twitter X" => 'https://x.com/eh_connard?s=21&t=gI8Bnbzdov4wuAo_5X6Yng',
+
     ],
     "footer_right" =>
     [
@@ -17,12 +22,25 @@ $footer = [
 ?>
 
 
+
+
+
 <footer>
     <?php foreach ($footer as $side) : ?>
         <div id="reseaux" class="div-footer">
             <ul>
                 <?php foreach ($side as $title => $url) : ?>
-                    <li><a href="<?= $url ?>"> <img src="" alt="Logo"><?= $title ?></a></li>
+                    <li><a href="<?= $url ?>">
+                            <img src="<?php if ($title == "Instagram"):?>public/pictogramme/instagram.svg 
+                     <?php elseif ($title == "TikTok"):?>public/pictogramme/tiktok.svg
+                     <?php elseif ($title == "Twitter X"):?>public/pictogramme/twitter-x.svg 
+                    <?php else:?>
+                      <?php endif ?>" alt="<?php if ($title == "Instagram"):?>Logo de Instagram
+                     <?php elseif ($title == "TikTok") :?>Logo de TikTok
+                     <?php elseif ($title == "Twitter X") :?>Logo de Twitter
+                    <?php else:?>
+                      <?php endif ?>">
+                            <?= $title ?></a> </li>
                 <?php endforeach ?>
             </ul>
         </div>
@@ -31,5 +49,11 @@ $footer = [
 
 
 
+
+
+
+
+
 </body>
+
 </html>
