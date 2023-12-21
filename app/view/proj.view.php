41b5457394f8c743565deed4f1106b548e174527
Sassounito <144890348+Sassounito@users.noreply.github.com>
2023-12-21 17:14:34 +0100

    proj css

--- app/view/proj.view.php
+++ app/view/proj.view.php
@@ -17,7 +17,7 @@
         <ul>
             <li id="slam" class="proj">
                 <figure>
-                    <figcaption>Notre slam : </figcaption>
+                    <figcaption><strong>Notre slam : </strong></figcaption>
                     <audio controls src="">
                         <a href=""> Download audio </a>
                     </audio>
@@ -25,13 +25,19 @@
                 <p id="text1">Plongez-vous dans le point de vue d’une femme qui a eu le malheur d’essayer de sortir tranquillement
                     dans la rue et qui finit par se faire emm*rder par des conn*rds.</p>
             </li>
-            <li id="video" class="proj">Voir notre vidéo de la campagne de sensibilisation : <a href="">Eh Connard !</a>
+            <li id="video" class="proj"><strong>Voir notre vidéo de la campagne de sensibilisation : </strong><a href="">Eh Connard !</a>
+                <p>
+                    <video width="640" height="360" controls>
+                        <source src=”http://techslides.com/demos/sample-videos/small.ogv” type=video/ogg>
+                        <source src="/build/videos/arcnet.io(7-sec).mp4" type=video/mp4>
+                    </video>
+                </p>
                 <p id="text2">Et si on prenait conscience de ce que l’on subit et qu’on faisait le choix de s’entraider et de mener
                     une révolte placée sous le signe de la sororité ?</p>
                 <p>La miss, la poupée, la jolie fille, la demoiselle, bref : </p>
-                <p><strong>LES GONZESSES SONT DE SORTIES !!!</strong></p>
+                <p><strong id="gonz">LES GONZESSES SONT DE SORTIES !!!</strong></p>
             </li>
-            <li id="lycee" class="proj">Visiter des lycées de France pour sensibiliser les jeunes au harcèlement de rue, répondre à leurs questions, etc…
+            <li id="lycee" class="proj"><strong>Visiter des lycées de France</strong> pour sensibiliser les jeunes au harcèlement de rue, répondre à leurs questions, etc…
                 <p>Eduquer c’est la meilleure façon de faire bouger les choses, apprendre aux jeunes que certains
                     comportements interiorisés ou banalisés, devraient être condamnés.
                     C’est pour ça que nous, les gonzesses, avons fait le choix de revêtir nos uniformes d’éducatrices
