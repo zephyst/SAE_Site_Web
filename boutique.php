33065a6d5eda2b6eff453c606a2954e31bb0088a
leabnl <144898323+leabnl@users.noreply.github.com>
2023-12-21 10:27:42 +0100

    changement boutique

--- boutique.php
+++ boutique.php
@@ -62,9 +62,20 @@
 <?php foreach ($boutique as $article) : ?>
 
     <div class= "article">
-        <p class= "nom" >
-        <p class="prix">
-        <p class= "description">
-        <p class= "images" >
+        <p class= "nom"><?= $article["nom"] ?> </p>
+        <p class="prix"><?= $article["prix"] ?> </p>
+        <p class= "description"><?= $article["description"] ?> </p>
+        <p class= "images" ><?= $article["images"] ?> </p>
    
-</div>
+        <select name="Quantite">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+
+
+
+    </div>
--- databaseboutique.php
+++ databaseboutique.php
@@ -3,20 +3,26 @@
 $boutique = [
   [
   	'nom' => 'SWEAT "LES GONZESSES SONT DE SORTIE"',
-    'description' => 'Sweat à capuche',
-    'prix' => 30,
+    'description' => 'Sweat à capuche floqué avec cordons. 
+    Poche kangourou, 
+    logo "LES GONZESSES SONT DE SORTIE" sur le devant. 
+    Lavable en machine à 40°.',
+    'prix' => '30€',
     'image' => "emplacement de l'image",
   ],
   [
-  	'nom' => 'T-SHIRT "LES GONZESSES SONT DE SORTIE"',
-    'description' => 'T-shirt ...',
-    'prix' => 20,
+  	'nom' => 'TASSE "LES GONZESSES SONT DE SORTIE"',
+    'description' => 'Tasse floqué. 
+    Logo "LES GONZESSES SONT DE SORTIE."',
+    'prix' => '15€',
     'image' => "emplacement de l'image", 
   ],
   [
   	'nom' => 'TOTE-BAG "LES GONZESSES SONT DE SORTIE"',
-    'description' => 'Tote bag',
-    'prix' => 10,
+    'description' => 'ToteBag floqué
+    Logo "LES GONZESSES SONT DE SORTIE" sur le devant.
+    Lavable en machine à 40°.',
+    'prix' => '10€',
     'image' => "emplacement de l'image",
   ],
 ];
