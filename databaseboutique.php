b58016f7b2d5c43a248daa26273ade3c3141e2b7
leabnl <144898323+leabnl@users.noreply.github.com>
2023-12-21 09:34:27 +0100

    boutique léa

---
+++ databaseboutique.php
@@ -0,0 +1,22 @@
+<?php 
+
+$boutique = [
+  [
+  	'nom' => 'SWEAT "LES GONZESSES SONT DE SORTIE"',
+    'description' => 'Sweat à capuche',
+    'prix' => 30,
+    'image' => "emplacement de l'image",
+  ],
+  [
+  	'nom' => 'T-SHIRT "LES GONZESSES SONT DE SORTIE"',
+    'description' => 'T-shirt ...',
+    'prix' => 20,
+    'image' => "emplacement de l'image", 
+  ],
+  [
+  	'nom' => 'TOTE-BAG "LES GONZESSES SONT DE SORTIE"',
+    'description' => 'Tote bag',
+    'prix' => 10,
+    'image' => "emplacement de l'image",
+  ],
+];
