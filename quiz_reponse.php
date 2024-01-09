1d0ec4fcbcb58acd1616fc391d6a8557eea57365
zephyst <130919837+zephyst@users.noreply.github.com>
2024-01-09 17:03:44 +0100

    les vrais bons chemins et noms

--- quiz_reponse.php
+++ quiz_reponse.php
@@ -3,7 +3,7 @@ $css="public/css/quizReponse.css";
 $page_title = 'Accueil/Quiz';
 
 ob_start();
-include "app/view/quizReponse.view.php";
+include "app/view/quizReponseView.php";
 $content = ob_get_clean();
 
 include "app/view/common/layout.php";
