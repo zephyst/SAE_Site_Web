ef184246908098d06e03ad951ce16a68cfc7882c
alexmx57 <alexmeux27@gmail.com>
2024-01-09 17:22:48 +0100

    début fiche alexandre

---
+++ app/view/ficheAlexandre.view.php
@@ -0,0 +1,28 @@
+<section id="first-block"><div id="black">
+    <p>Présentation Alexandre Meux</p>
+    <img src="public/images/PhotoAlexandre.jpg">
+</div>
+
+<div id="burly">
+    <p>DEVELOPPEUR WEB</p>
+    <img src="https://www.bew-web-agency.fr/wp-content/uploads/2022/01/developpeur-web.jpg">
+</div>
+</section>
+<div id="black2">
+    <h1>ALLER VOIR MON PORTFOLIO AVEC TOUS MES PROJETS !</h1>
+    <section><p>MES PROJETS</p></section>
+</div>
+<div id="presentation">
+    <h2>DESCRIPTION</h2>
+    <p></p>
+</div>
+
+<div id="contact">
+    <h4>Mes réseaux personnels:</h4>
+    <ul>
+        <li><a href="">Twitter</a></li>
+        <li><a href="">TikTok</a></li>
+        <li><a href="">Instagram</a></li>
+        <li><a href="">Facebook</a></li>
+    </ul>
+</div>
