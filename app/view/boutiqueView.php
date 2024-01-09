76511ae6f2c4f577b97ac53c076f21b70ed48372
zephyst <130919837+zephyst@users.noreply.github.com>
2024-01-09 17:54:51 +0100

    chemins

--- app/view/boutiqueView.php
+++ app/view/boutiqueView.php
@@ -2,23 +2,29 @@
 
 
 <?php foreach ($boutique as $article) : ?>
+    <div class="article">
+        <img src="<?= $article["image"] ?>"
+        <div class= "content">
+            <div class="content">
+                <p class= "nom" > <?= $article["nom"] ?> </p>
+                <p class="prix"><?= $article["prix"] ?></p>
+                <p class= "description"><?= $article["description"] ?></p>
+                <div class="selecteurs">
+                    <div class="selecteurs">
+                        <?= $article["taille"] ?>
+                        <select name="Quantite">
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                </div>
 
-    <div class= "article"> 
-        <p class= "nom" > <?= $article["nom"] ?> </p>
-        <p class="prix"><?= $article["prix"] ?></p>
-        <p class= "description"><?= $article["description"] ?></p>
-        <p class="taille" ><?= $article["taille"] ?> </p>
-        <select name="QuantiteTShirt">
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-        </select>
-</div>
+            </div>
 
-<div class="photo">
-<p class= "images" >  <img src="<?= $article["image"] ?>" </p>
-</div>
-<?php endforeach ?> 
+        </div>
+    </div>
+<?php endforeach ?>
