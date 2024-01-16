f65440855b597b8b3bf18260c26a63e7485902e5
zephyst <130919837+zephyst@users.noreply.github.com>
2024-01-16 19:24:52 +0100

    projet avancé

--- app/view/projetsView.php
+++ app/view/projetsView.php
@@ -1,28 +1,39 @@
 <h1>Nos projets : </h1>
-<ul>
-    <li id="slam" class="proj">
-        <figure>
-            <figcaption><strong>Notre slam : </strong></figcaption>
-            <audio controls src="public/sounds/Slam.wav" id="audio">
-                <a href=""> Download audio </a>
-            </audio>
-        </figure>
-        <p id="text1">Plongez-vous dans le point de vue d’une femme qui a eu le malheur d’essayer de sortir tranquillement
-            dans la rue et qui finit par se faire emm*rder par des conn*rds.</p>
-    </li>
-    <li id="video" class="proj">
-        <p id="title"><strong>Notre campagne de sensibilisation : </strong>Eh Connard !</p>
-        <iframe width="585" height="300" src="public/images/Spot_Sensibilisation_EhConnard.mp4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-            
-        <div id="short-sheet">
-            <p id="text2">Et si on prenait conscience de ce que l’on subit et qu’on faisait le choix de s’entraider et de mener
-                une révolte placée sous le signe de la sororité ?<br>La miss, la poupée, la jolie fille, la demoiselle, bref : </p>
-            <p><strong id="gonz">LES GONZESSES SONT DE SORTIES !!!</strong></p>
-        </div>
-    </li>
-    <li id="lycee" class="proj"><strong>Visiter des lycées de France</strong> pour sensibiliser les jeunes au harcèlement de rue, répondre à leurs questions, etc…
-        <p>Eduquer c’est la meilleure façon de faire bouger les choses, apprendre aux jeunes que certains comportements interiorisés ou banalisés, devraient être condamnés.<br>C’est pour ça que nous, les gonzesses, avons fait le choix de revêtir nos uniformes d’éducatrices afin d’agir pour le bien de toutes. </p>
-    </li>
-</ul>
+<section id="slam" class="proj">
+    <h3>Notre slam :</h3>
+    <div class="slamContent">
+        <audio controls src="public/sounds/Slam.wav" id="audio"></audio>
+        <p id="textSlam">
+            Plongez-vous dans le point de vue d’une femme qui a eu le malheur d’essayer de sortir tranquillement
+            dans la rue et qui finit par se faire emm*rder par des conn*rds.
+        </p>
+    </div>
+
+</section>
+
+<section id="video" class="proj">
+    <h3 id="title">
+        <strong>Notre campagne de sensibilisation : </strong>
+        Eh Connard !
+    </h3>
+    <iframe width="532" height="300" src="public/images/Spot_Sensibilisation_EhConnard.mp4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <div id="short-sheet">
+        <p id="textVideo">
+            Et si on prenait conscience de ce que l’on subit et qu’on faisait le choix de s’entraider et de mener
+            une révolte placée sous le signe de la sororité ?<br>
+            La miss, la poupée, la jolie fille, la demoiselle, bref :
+        </p>
+        <h4>LES GONZESSES SONT DE SORTIES !!!</h4>
+    </div>
+</section>
+
+<section id="lycee" class="proj">
+    <p>
+        <strong>Visiter des lycées de France</strong> pour sensibiliser les jeunes au harcèlement de rue, répondre à leurs questions, etc…
+    </p>
+    <p>Eduquer c’est la meilleure façon de faire bouger les choses, apprendre aux jeunes que certains comportements interiorisés ou banalisés, devraient être condamnés.<br>C’est pour ça que nous, les gonzesses, avons fait le choix de revêtir nos uniformes d’éducatrices afin d’agir pour le bien de toutes. </p>
+</section>
+
 <h3>Soutenez nous avec le hashtag <strong>#EhConnard</strong> !</h3>
