1cd9923f1f389b5948f4549be0e75a00aac3e5c3
leabnl <144898323+leabnl@users.noreply.github.com>
2024-01-18 16:48:39 +0100

    je vais péter mon crâne

--- ficheLea.php
+++ ficheLea.php
@@ -3,59 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/lea.css"
-</head>
-
+    <link rel="stylesheet" href="public/css/lea.css">
 
 <header>
-    <div class="barre nav">
-        <img src="public/images/LOGO-Eh!CONNARD.png" alt="logoEh!Connard">
+    <div class="navbar-left">
+            <img src="public/images/LOGO-Eh!CONNARD.png" alt="logoEh!Connard">
     </div>
-    
-    <ul>
-        <li>
-            <a href="contact.php"> Nous contacter </a>
-        </li>
-        <li>
-            <a href="boutique.php"> Boutique </a>
-        </li>
-        <li>
-            <a href="page_projet.php"> Nos projets </a>
-        </li>
-        <li>
-            <a href="trombi.php"> Qui sommes nous ? </a>
-        </li>
-    </ul>
+    <div class="navbar-right">
+        <a href="./trombi.php">Revenir au site principal</a>
+    </div>
+
 </header>
 
 
 
 <body>
-    <header>
+    <div class="titre">
+
+    <main>
+    
         <h1>Léa BOINEL</h1>
         <h2>Créatrice de Merch</h2>
-     </header> 
- <main>
-       
-
-        <img src=""> mettre un dessin
-        
+ 
 
 
         <div class="Mes Passions">
             <h2> Mes Passions:</h2>
                 <p>
-               // parler de mes passions
+                La vidéo et l'animation me fascinent depuis des années.
+                La vidéo, par sa capacité à capturer la réalité, documentant le quotidien ou explorant des horizons imaginaires. Elle est le témoin de notre histoire et le miroir de nos émotions.
+                L'animation, quant à elle, va au-delà des limites de la réalité, donnant vie à des idées abstraites et à des univers fantastiques. Des personnages prennent vie, des histoires prennent forme, emportant le spectateur dans un voyage visuel envoûtant.
+                 Ce sont des expériences sensorielle profonde, suscitant des émotions et inspirant la créativité. Pour ceux qui les chérissent, la vidéo et l'animation sont bien plus que des loisirs ; ce sont des moyens d'exploration infinie et d'expression artistique sans limites.
 
                 </p>
     
         </div>     
         
-        <div class="Mon Travail">
+        <div class="Mon Travail"> 
             <h2>Mon Travail:</h2>
             
                 <p>
-                    //parler de mon travail
+                Mon travail est l'extension de mon imagination débordante, une exploration infinie capturée dans des vidéos, photos et animations que je partage avec passion sur Instagram. À travers ces médias, je façonne des instants mémorables, cherchant à susciter des souvenirs et des émotions chez ceux qui découvrent mon travail.
 
                 </p>
 
@@ -63,7 +51,7 @@
             <h2>Qui Je suis ?:</h2>
             
                 <p>
-                    //qui je suis rôle dans l'équipe
+                    Je m'appelle Léa Boinel, je m'occupe du merch de notre association 
 
                 </p>
 
@@ -71,7 +59,7 @@
             <h2>Nos Réseaux:</h2>
 
                 <p>
-                    instagram : @lea.bnl // compléter
+                    instagram : @lea.bnl // 
                 </p>
               
         </div>  
