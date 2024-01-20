54818f66f6ef72ecae3ac658689295f703868400
leabnl <144898323+leabnl@users.noreply.github.com>
2024-01-20 19:27:53 +0100

    j'y suis depuis 14h

--- ficheLea.php
+++ ficheLea.php
@@ -7,7 +7,7 @@
 
 <header>
     <div class="navbar-left">
-            <img src="public/images/LOGO-Eh!CONNARD.png" alt="logoEh!Connard">
+            <img src="public/images/EH!CONNARD.png" alt="Logo_EH!CONNARD">
     </div>
     <div class="navbar-right">
         <a href="./trombi.php">Revenir au site principal</a>
@@ -15,56 +15,42 @@
 
 </header>
 
-
-
+        <section id=dessin>
+        <img src="public/images/gonz.png" id="dessin">
 <body>
-    <div class="titre">
 
-    <main>
-    
-        <h1>Léa BOINEL</h1>
+        
+<h1>Léa Boinel</h1>
         <h2>Créatrice de Merch</h2>
- 
 
+    <div class="container">
+       
+        
 
-        <div class="Mes Passions">
-            <h2> Mes Passions:</h2>
-                <p>
-                La vidéo et l'animation me fascinent depuis des années.
+        <div class="box">
+            <h3>Mes passions</h3>
+            <p>La vidéo et l'animation me fascinent depuis des années.
                 La vidéo, par sa capacité à capturer la réalité, documentant le quotidien ou explorant des horizons imaginaires. Elle est le témoin de notre histoire et le miroir de nos émotions.
                 L'animation, quant à elle, va au-delà des limites de la réalité, donnant vie à des idées abstraites et à des univers fantastiques. Des personnages prennent vie, des histoires prennent forme, emportant le spectateur dans un voyage visuel envoûtant.
-                 Ce sont des expériences sensorielle profonde, suscitant des émotions et inspirant la créativité. Pour ceux qui les chérissent, la vidéo et l'animation sont bien plus que des loisirs ; ce sont des moyens d'exploration infinie et d'expression artistique sans limites.
+                 Ce sont des expériences sensorielle profonde, suscitant des émotions et inspirant la créativité. Pour ceux qui les chérissent, la vidéo et l'animation sont bien plus que des loisirs ; ce sont des moyens d'exploration infinie et d'expression artistique sans limites</p>
+        </div>
 
-                </p>
-    
-        </div>     
-        
-        <div class="Mon Travail"> 
-            <h2>Mon Travail:</h2>
-            
-                <p>
-                Mon travail est l'extension de mon imagination débordante, une exploration infinie capturée dans des vidéos, photos et animations que je partage avec passion sur Instagram. À travers ces médias, je façonne des instants mémorables, cherchant à susciter des souvenirs et des émotions chez ceux qui découvrent mon travail.
+        <div class="box">
+            <h3>Mon travail</h3>
+            <p>Mon travail est l'extension de mon imagination débordante, une exploration infinie capturée dans des vidéos, photos et animations que je partage avec passion sur Instagram. À travers ces médias, je façonne des instants mémorables, cherchant à susciter des souvenirs et des émotions chez ceux qui découvrent mon travail.</p>
+        </div>
 
-                </p>
+        <div class="box">
+            <h3>Qui je suis</h3>
+            <p>  Je m'appelle Léa Boinel, je suis en charge de la création des designs qu'on retrouve dans la boutique de l'association.</p>
+        </div>
+    </div>
 
-        <div class="Qui Je suis ?">
-            <h2>Qui Je suis ?:</h2>
-            
-                <p>
-                    Je m'appelle Léa Boinel, je m'occupe du merch de notre association 
-
-                </p>
-
-        <div class="Nos Réseaux">
-            <h2>Nos Réseaux:</h2>
-
-                <p>
-                    instagram : @lea.bnl // 
-                </p>
-              
-        </div>  
-
+    <footer>
+        <div class="social-icons">
+            <a href="https://www.instagram.com/lea_bnl_/"><img src="public/images/icon.png" alt="Instagram">lea_bnl_</a>
+            <a href="https://www.instagram.com/eh_connard/"><img src="public/images/icon.png" alt="Instagram">eh_connard</a>
+        </div>
+    </footer>
 </body>
-
-
 </html>
