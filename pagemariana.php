ffc5823d86453c9ce7d1ebac9528666852bac3d9
marianaxxe <marianaa7704@gmail.com>
2024-01-23 09:09:27 +0100

    page marianaxxe fini

--- pagemariana.php
+++ pagemariana.php
@@ -1,15 +1,3 @@
-<?php
-
-$logo = 'logomariana.png';
-$header = [
-    'Nous contacter' => 'contact.php',
-    'Boutique' => 'boutique.php',
-    'Nos projets' => 'page_projet.php',
-    'Qui sommes nous ?' => 'trombi.php',
-];
-
-?>
-
 <html lang="fr">
 
 <head>
@@ -36,8 +24,12 @@ $header = [
 <main>
 
 <section id="identité">
-  <h1>Mariana, la graphiste</h1>
-  <img src="public/images/photo1Mariana.jepg" alt="photo de Mariana"> 
+  <h1>Mariana la graphiste</h1>
+  <div id="photo">
+  <img src="public/images/marianaphoto.jpeg" alt="photo de Mariana" >
+  </div>
+  
+
   
   <h2>alias l'animatrice</h2>
 
@@ -51,21 +43,62 @@ $header = [
   </div>
 
   <div class="idee">
-    <h3>Ce que j'aime :</h3>
-    <p>
-      <ul>
-        <li>J'aime beaucoup le maquillage, il y a un univers où j'en aurai fait mon métier je pense.</li>
-        <li>J'aime TOUT organiser. C'est bien pour ça que je veux travailler dans l'évenementiel. Par exemple, quand je pars en vacances avec des amis je vais crée un diaporama sur notre voyage.</li>
-        <li>J'aime les souvenirs, les photos. Etant quelqu'un de nostalgie j'aime garder des souvenirs. J'en reviens à faire des vidéos souvenirs annuelles à chaque mois décembre. </li>
     
-      </ul>
-    </p>
+      <h3>Ce que j'aime :</h3>
+        <p>
+          <ul>
+             <li>J'aime beaucoup le maquillage, il y a un univers où j'en aurai fait mon métier je pense.</li>
+            <li>J'aime TOUT organiser. C'est bien pour ça que je veux travailler dans l'évenementiel. Par exemple, quand je pars en vacances avec des amis je vais crée un diaporama sur notre voyage.</li>
+            <li>J'aime les souvenirs, les photos. Etant quelqu'un de nostalgique j'aime garder des souvenirs. J'en reviens à faire des vidéos souvenirs annuelles à chaque mois décembre. </li>
+    
+          </ul>
+      </p>
+   
+    
 
+    <div id="photos" >
+        <img src="public/images/makeup.jpeg" alt="maquillage fait par moi">
+        <img src="public/images/makeup2.jpeg" alt="maquillage fait par moi">
+        <img src="public/images/makeup3.jpeg" alt="maquillage fait par moi">
+  
+    </div>
+
+  </div>
+
+  <div class="idee">
+  <h3>Une envie de voir mes réseaux ? </h3>
+  <ul >
+    <li class="reseaux"><a href="https://www.instagram.com/gll_mariana/">Vous pouvez me retrouver sur Instagram : @gll_mariana</a></li>
+    <li class="reseaux"><a href="https://www.tiktok.com/@marianaxxe?is_from_webapp=1&sender_device=pc">Et vous pouvez aussi me retrouver sur TikTok : @marianaxxe</a></li>
+  </ul>
   </div>
 </section>
 
-<section id="contact">
+
+  
 
 
 </section>
 </main>
+
+<footer>
+  <div class="footer">
+    <ul>
+      <li><a href="https://www.instagram.com/eh_connard/?igshid=OGQ5ZDc2ODk2ZA%3D%3D"><img src="public/pictogramme/instagram.svg" alt="logo d'Instagram">Instagram</a></li>
+      <li><a href="https://www.tiktok.com/@eh_connard?_t=8hva9LT0cTI&_r=1"><img src="public/pictogramme/tiktok.svg " alt="logo de Tiktok">TikTok</a></li>
+      <li><a href="https://x.com/eh_connard?s=21&t=gI8Bnbzdov4wuAo_5X6Yng"><img src="public/pictogramme/twitter-x.svg  " alt="logo de Twitter/X">Twitter/X</a></li>
+
+    </ul>
+
+  </div>
+
+  <div class="footer">
+    <ul>
+      <li><a href="">Nous contacter</a></li>
+      <li><a href="">Conditions générales de ventes</a></li>
+      <li><a href="">Politique de confidentialité</a></li>
+    </ul>
+
+  </div>
+
+</footer>
